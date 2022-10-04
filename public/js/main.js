6aebe71752d6bb877fabf6001cf0c3607d6fb928
kevinderitis <kevin.deritis@hotmail.com>
2022-10-04 20:44:59 -0300

    Changes 04/10

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const socket = io.connect();
+
+const input = document.querySelector('input')
+document.querySelector('button').addEventListener('click', () => {
+    socket.emit('mensaje', input.value);
+})
+
+socket.on('mensajes', msjs => {
+    const mensajesHTML = msjs
+        .map(msj => `SocketId: ${msj.socketid} -> Mensaje: ${msj.mensaje}`)
+        .join('<br>')
+    document.querySelector('p').innerHTML = mensajesHTML
+});
+
+
